62ddf777200ad289113490347aac503a63933d27
Michael Trumbull <michael.trumbull@nscc.ca>
2025-09-23 10:26:15 -0300

    Working on Views.

--- PhotoAlbum/Controllers/HomeController.cs
+++ PhotoAlbum/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace PhotoAlbum.Controllers
 
         public IActionResult Index()
         {
-            // List of 3 photos 
+            // List of 2 photos 
             List<Photo> photos = new List<Photo>();
 
             // Create 2 photos
@@ -25,14 +25,14 @@ namespace PhotoAlbum.Controllers
             photo1.Title = "Penny";
             photo1.Description = "My cat Penny, loves sitting in the window sill";
             photo1.Filename = "penny.jpg";
-            photo1.PublishDate = DateTime.Now;
+            photo1.PublishDate = new DateTime(2020, 12, 20);
 
             Photo photo2 = new Photo();
             photo2.PhotoId = 2;
             photo2.Title = "Audrey";
             photo2.Description = "My pug Audrey loves sleeping on the couch.";
             photo2.Filename = "audrey.jpg";
-            photo2.PublishDate = DateTime.Now;
+            photo2.PublishDate = new DateTime(2024, 09, 30, 9, 30, 0);
 
             photos.Add(photo1);
             photos.Add(photo2);
@@ -48,7 +48,25 @@ namespace PhotoAlbum.Controllers
         {
             int photoId = id;
 
-            return View();
+            Photo photo = new Photo();
+            photo.PhotoId = photoId;
+
+            if (photo.PhotoId == 1)
+            {
+                photo.Title = "Penny";
+                photo.Description = "My cat Penny, loves sitting in the window sill";
+                photo.Filename = "penny.jpg";
+                photo.PublishDate = new DateTime(2020, 12, 20);
+            }
+            else
+            {
+                photo.Title = "Audrey";
+                photo.Description = "My pug Audrey loves sleeping on the couch.";
+                photo.Filename = "audrey.jpg";
+                photo.PublishDate = new DateTime(2024, 09, 30, 9, 30, 0);
+            }
+
+            return View(photo);
         }
     }
 }
--- PhotoAlbum/Models/Photo.cs
+++ PhotoAlbum/Models/Photo.cs
@@ -1,8 +1,11 @@
-﻿namespace PhotoAlbum.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoAlbum.Models
 {
     public class Photo
     {
         // Primary key
+        [Display(Name = "Photo Id")]
         public int PhotoId { get; set; }
 
         public string Title { get; set; } = string.Empty;
@@ -12,6 +15,7 @@
         // The photo's filename on the server
         public string Filename { get; set; } = string.Empty;
 
+        [Display(Name = "Published")]
         public DateTime PublishDate { get; set; }
 
         // Navigation Property (code)
